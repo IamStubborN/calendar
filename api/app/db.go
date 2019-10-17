c2b9543ebaa5bce0bf47100ed368dbe4d4b79b03
IamStubborN <istubb@me.com>
2019-10-17 09:49:10 +0300

    [Done] Migrations replace from code to docker-compose

--- api/app/db.go
+++ api/app/db.go
@@ -7,7 +7,6 @@ import (
 	"github.com/IamStubborN/calendar/api/config"
 	"github.com/IamStubborN/calendar/api/pkg/logger"
 	"github.com/jmoiron/sqlx"
-	migrate "github.com/rubenv/sql-migrate"
 )
 
 func initializeSQLConn(cfg *config.Config, logger logger.UseCase) (*sqlx.DB, error) {
@@ -19,8 +18,6 @@ func initializeSQLConn(cfg *config.Config, logger logger.UseCase) (*sqlx.DB, err
 	if err := retryConnect(pool, cfg.Storage.Retry, logger); err != nil {
 		return nil, err
 	}
-
-	migrationLogic(pool, logger)
 
 	return pool, nil
 }
@@ -49,17 +46,4 @@ func retryConnect(pool *sqlx.DB, fatalRetry int, logger logger.UseCase) error {
 	}
 
 	return nil
-}
-
-func migrationLogic(db *sqlx.DB, logger logger.UseCase) {
-	migrations := &migrate.FileMigrationSource{
-		Dir: "migrations",
-	}
-
-	_, err := migrate.Exec(db.DB, "postgres", migrations, migrate.Up)
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	logger.Info("migrations complete")
 }
